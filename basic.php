a50114cd05bcb630e305fcf77de774f06e6991a2
vladzur <vladzur@gmail.com>
2017-06-07 21:53:01 -0400

    Script funny messages

--- basic.php
+++ basic.php
@@ -6,7 +6,7 @@ $i = 0;
 
 $app = function ($request, $response) use (&$i) {
     $i++;
-    $text = "This is request number $i.\n";
+    $text = "This is request number $i. I'm not just a classic php script\n";
     $headers = array('Content-Type' => 'text/plain');
     $response->writeHead(200, $headers);
     $response->end($text);
@@ -19,4 +19,8 @@ $http = new React\Http\Server($socket);
 $http->on('request', $app);
 
 $socket->listen(1337);
+
+echo "Socket is listening on port 1337\n";
+echo "Open in browser 127.0.0.1:1337\n";
+
 $loop->run();
